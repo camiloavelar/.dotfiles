d5123321fed33e2f744ff2ff5c02ec9fde2f21a4
CamiloAvelar <camilo.avelar@gmail.com>
2023-12-10 21:51:45 -0300

    feat(touchpad): add natural scrolling

--- i3/.config/i3/bin/xinput.sh
+++ i3/.config/i3/bin/xinput.sh
@@ -2,3 +2,4 @@
 # Set mouse speed
 xinput set-prop 25 216 0.35 0 0 0 0.35 0 0 0 1
 xinput set-prop "DELL08EA:00 06CB:CCA8 Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "DELL08EA:00 06CB:CCA8 Touchpad" "libinput Natural Scrolling Enabled" 1
