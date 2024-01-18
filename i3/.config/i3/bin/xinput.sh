e75cd4990fe1e049fabe3ba529829ace302f9e43
CamiloAvelar <camilo.avelar@gmail.com>
2024-01-18 18:25:40 -0300

    fix(mouse): set by name

--- i3/.config/i3/bin/xinput.sh
+++ i3/.config/i3/bin/xinput.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Set mouse speed
-xinput set-prop 25 216 0.35 0 0 0 0.35 0 0 0 1
+xinput set-prop "BenQ ZOWIE BenQ ZOWIE Gaming Mouse" 216 0.25 0 0 0 0.25 0 0 0 1
 xinput set-prop "DELL08EA:00 06CB:CCA8 Touchpad" "libinput Tapping Enabled" 1
 xinput set-prop "DELL08EA:00 06CB:CCA8 Touchpad" "libinput Natural Scrolling Enabled" 1
