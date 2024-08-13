4248afb2ef10b7984cd3d333e8110f3da03c3a7a
Camilo Avelar <camilo.avelar@avenue.us>
2024-08-13 14:13:09 -0300

    refactor(wezterm): adding configs

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,29 +5,31 @@ local mux = wezterm.mux
 
 config.font = wezterm.font("JetbrainsMono Nerd Font")
 config.font_size = 14
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.window_background_opacity = 0.9
 config.disable_default_key_bindings = true
 config.window_decorations = "RESIZE"
 config.colors = {
-  foreground = '#cdd6f4',
-  background = 'black',
+	foreground = "#cdd6f4",
+	background = "black",
 }
 
 wezterm.on("gui-startup", function(cmd)
-  local tab, pane, window = mux.spawn_window(cmd or {})
-  window:gui_window():toggle_fullscreen()
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():toggle_fullscreen()
 end)
 
 config.window_padding = {
-  left = 2,
-  right = 2,
-  top = 0,
-  bottom = 0,
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
 }
 
 config.keys = {
+	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 	{ key = "q", mods = "CTRL", action = act.QuitApplication },
 	{ key = "q", mods = "SHIFT|CTRL", action = act.QuitApplication },
 	{ key = "Enter", mods = "ALT", action = act.ToggleFullScreen },
